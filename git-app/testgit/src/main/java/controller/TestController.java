611568792ee70ff083052fb9da645c29e6466e7f
zhangchen <15884216715@163.com>
2020-01-09 14:18:47 +0800

    提交项目

---
+++ git-app/testgit/src/main/java/controller/TestController.java
@@ -0,0 +1,16 @@
+package controller;
+
+/**
+ * @ClassName MrSwagger2Config
+ * @Description: TODO
+ * @Author zhangchen
+ * @Date {date}
+ * @Version V1.0
+ **/
+public class TestController {
+
+
+    public static void main(String[] args) {
+        System.out.println("这是我的项目");
+    }
+}
